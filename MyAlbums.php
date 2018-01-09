1da6c16afc776425090939cd8382aa7c138cf06b
debo0030 <debo0030@algonquinlive.com>
2018-01-08 23:16:12 -0500

    starter for My Albums page

--- MyAlbums.php
+++ MyAlbums.php
@@ -7,6 +7,14 @@
 
     <div class="container text-center">
         <h2>My Albums</h2><br />
+        <p>Welcome, <?php echo User::find_by_id($session->user_id)->name; ?>!</p>
+        <a href="AddAlbum.php">Create New Album</a>
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post"  enctype="multipart/form-data">
+            
+           
+            <br/><br/>
+            <input type="submit" name="btnUpload" value="Upload" class="btn btn-lg btn-success"/>
+       </form> 
     </div>
 
 <?php include("includes/footer.php");
